691ef918a03deb695f70c57f4a3e1d6735ecff7f
Diegof3214 <diegof3214@hotmail.com>
2025-12-09 22:03:27 -0500

    Conectar frontend a producción

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,18 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://127.0.0.1:8000/api/';
+// 👇 PEGA AQUÍ TU URL DE RENDER (Asegúrate de que termine en /api/)
+const API_URL = "https://gamilearn-backend.onrender.com/api/"; 
 
 const api = axios.create({
   baseURL: API_URL,
 });
 
-// Agregar automáticamente el token JWT a todas las peticiones
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('access');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
